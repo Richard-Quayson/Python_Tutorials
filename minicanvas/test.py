f21b662dbee18009cf63be1e7bc3a5adf6d9f724
Richard Kweenu Nana Quayson <richard.quayson@ashesi.edu.gh>
2023-11-22 23:50:48 +0000

    Updated test file

--- minicanvas/test.py
+++ minicanvas/test.py
@@ -1,23 +1,6 @@
 from minicanvas import Ashesian, Faculty, Student, NotCanvas
 
 
-# a1 = Ashesian("12342026", "Kofi Senam", "Ghanaian")
-# print(a1)
-
-# s1 = Student("12342026", "Richard Quayson", "Ghanaian", "CE", "2026", 2.0)
-# print(s1)
-
-# f1 = Faculty("23452020", "Dennis Owusu", "Ghanaian", "Computer Science and Information Systems")
-# print(f1)
-
-# print("\nAdding Ashesi Success to student1")
-# s1.add_course("Asheesi Success")
-# print(s1)
-
-# print("\nChanging student1's name to Comfort")
-# s1.change_name("Comfort Quayson")
-# print(s1)
-
 app = NotCanvas()
 choice = app.menu()
 while choice != "4":
